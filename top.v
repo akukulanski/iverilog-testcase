15a4e32e2af6bf8012214691c2f150a3a158250a
akukulanski <akukulanski@gmail.com>
2020-01-24 22:26:08 -0300

    deleted comments

--- top.v
+++ top.v
@@ -7,33 +7,17 @@ output x;
 reg x;
 reg [1:0] \fsm_state$114 = 0;
 
-// always @* begin
-//     if (clk)
-//         x = 1'b0;
-//     else
-//         x = 1'b1;
-// end
-
   always @(posedge clk)
       \fsm_state$114  <= \fsm_state$114 + 2'h1;
   always @* begin
     x = 1'h0;
-    /* src = "/home/kuku/STORAGE/estructura/Personales/Desarrollo/repositories/cores-nmigen/cores/axi_lite.py:67" */
     casez (\fsm_state$114 )
-      /* src = "/home/kuku/STORAGE/estructura/Personales/Desarrollo/repositories/cores-nmigen/cores/axi_lite.py:68" */
-      /* \nmigen.decoding  = "IDLE/0" */
       2'h0:
           x = 1'h1;
-      /* src = "/home/kuku/STORAGE/estructura/Personales/Desarrollo/repositories/cores-nmigen/cores/axi_lite.py:79" */
-      /* \nmigen.decoding  = "WAITING_DATA/2" */
       2'h2:
           x = 1'h0;
-      /* src = "/home/kuku/STORAGE/estructura/Personales/Desarrollo/repositories/cores-nmigen/cores/axi_lite.py:86" */
-      /* \nmigen.decoding  = "WAITING_ADDR/3" */
       2'h3:
           x = 1'h1;
-      /* src = "/home/kuku/STORAGE/estructura/Personales/Desarrollo/repositories/cores-nmigen/cores/axi_lite.py:93" */
-      /* \nmigen.decoding  = "DONE/1" */
       2'h1:
           x = 1'h0;
     endcase
