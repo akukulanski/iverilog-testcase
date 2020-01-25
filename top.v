7d194a1c8852afdf4e488efd647284d517602433
akukulanski <akukulanski@gmail.com>
2020-01-24 22:31:53 -0300

    reduced logic

--- top.v
+++ top.v
@@ -5,23 +5,20 @@ module top (clk, x);
 input clk;
 output x;
 reg x;
-reg [1:0] \fsm_state$114 = 0;
+reg \fsm_state$114 = 1'h0;
 
-  always @(posedge clk)
-      \fsm_state$114  <= \fsm_state$114 + 2'h1;
-  always @* begin
+always @(posedge clk)
+    \fsm_state$114  <= \fsm_state$114 + 1'h1;
+
+always @* begin
     x = 1'h0;
     casez (\fsm_state$114 )
-      2'h0:
+      1'h0:
           x = 1'h1;
-      2'h2:
-          x = 1'h0;
-      2'h3:
-          x = 1'h1;
-      2'h1:
+      1'h1:
           x = 1'h0;
     endcase
-  end
+end
 
 `ifdef COCOTB_SIM
 initial begin
