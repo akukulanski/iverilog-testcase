92e0db90ccd789a7d589a6c18a6a8b1ec7db43ab
akukulanski <akukulanski@gmail.com>
2020-01-24 22:35:06 -0300

    easier name

--- top.v
+++ top.v
@@ -5,14 +5,14 @@ module top (clk, x);
 input clk;
 output x;
 reg x;
-reg \fsm_state$114 = 1'h0;
+reg y = 1'h0;
 
 always @(posedge clk)
-    \fsm_state$114  <= \fsm_state$114 + 1'h1;
+    y  <= y + 1'h1;
 
 always @* begin
     x = 1'h0;
-    casez (\fsm_state$114 )
+    casez (y )
       1'h0:
           x = 1'h1;
       1'h1:
