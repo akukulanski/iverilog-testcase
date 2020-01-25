4b2c2e4ca6f28679ba6b7379945b00f655751fc5
akukulanski <akukulanski@gmail.com>
2020-01-24 22:47:27 -0300

    test waiting one more rising edge

--- test/test.py
+++ test/test.py
@@ -5,16 +5,25 @@ from cocotb.clock import Clock
 CLK_PERIOD_BASE = 10
 
 @cocotb.test()
-def test_x(dut):
+def test_a(dut):
 
     dut.clk <= 0
     
     cocotb.fork(Clock(dut.clk, 10, 'ns').start())
 
-    #yield RisingEdge(dut.clk)
+    for _ in range(20):
+        yield RisingEdge(dut.clk)
+        val = dut.x.value.integer
+
+@cocotb.test()
+def test_b(dut):
+
+    dut.clk <= 0
+    
+    cocotb.fork(Clock(dut.clk, 10, 'ns').start())
+
+    yield RisingEdge(dut.clk)
 
     for _ in range(20):
         yield RisingEdge(dut.clk)
         val = dut.x.value.integer
-        #assert dut.x.value.integer != last, 'dut.x.value == {}'.format(dut.x.value)
-        #last = dut.x.value.integer
